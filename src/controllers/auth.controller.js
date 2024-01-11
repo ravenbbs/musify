abd56f1093722d79a92c4a3f88eb8e573a636449
Julio Condor <julioc3bgud@gmail.com>
2024-01-10 21:44:48 -0500

    Callback module

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -21,9 +21,9 @@ const auth = (req, res) => {
   res.cookie(apiConfig.STATE_KEY, state);
 
   res.redirect(
-    'https://accounts.spotify.com/authorize?' +
+    "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
-        response_type: 'code',
+        response_type: "code",
         client_id: apiConfig.CLIENT_ID,
         scope: apiConfig.SCOPE,
         redirect_uri: apiConfig.REDIRECT_URI,
@@ -32,6 +32,22 @@ const auth = (req, res) => {
   );
 };
 
+const callback = async (req, res) => {
+  const MILLISECONDS = 1000;
+  const ONE_WEEK = 604800000;
+
+  const { code = null, state = null, error = null } = req.query;
+
+  const /**{string} */ storedState = req.cookies[apiConfig.STATE_KEY];
+
+  if (error || !state || state !== storedState) {
+    return res.redirect("/login");
+  } else {
+    res.clearCookie(apiConfig.STATE_KEY);
+  }
+};
+
 module.exports = {
   auth,
+  callback
 };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -13,8 +13,9 @@ const router = require("express").Router();
 /**
  * custom modules
  */
-const { auth } = require('../controllers/auth.controller')
+const { auth, callback } = require('../controllers/auth.controller')
 
 router.get('/', auth)
+router.get('/callback', callback)
 
 module.exports = router;
