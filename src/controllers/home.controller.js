28fb2db668b6a6c7f2cffd72ffaabb161a2855a7
Julio Condor <julioc3bgud@gmail.com>
2024-01-13 00:49:31 -0500

    recently played from api - add to home controller

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -8,14 +8,30 @@
 /**
  * custom modules
  */
-
+const apiConfig = require("../config/api.config")
 const userApi = require("../api/user.api");
+const playerApi = require("../api/player.api");
+const trackApi = require("../api/track.api");
 
 const home = async (req, res) => {
   //current user profile
   const currentProfile = await userApi.getProfile(req);
+
+  //recently played
+  const recentlyPlayed = await playerApi.getRecentlyPlayed(req);
+  const recentlyPlayedTracks = await recentlyPlayed.items.map(
+    ({ track }) => track
+  );
+
+  //recommended albums
+  const trackIds = recentlyPlayedTracks.map(({ id }) => id);
+  const trackSeed = trackIds.slice(0, 5).join(',')
+  const recommendedAlbums = await trackApi.getRecommendedTrack(req, trackSeed, apiConfig.LOW_LIMIT )
+
   res.render("./pages/home", {
     currentProfile,
+    // recentlyPlayedTracks,
+    recommendedAlbums
   });
 };
 
