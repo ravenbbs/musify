a4abcbf8873f0ef620fc5cd3dbe1aff83a276343
Julio Condor <julioc3bgud@gmail.com>
2024-01-14 21:29:11 -0500

    Get data track session -

---
+++ public/js/player/client_player.api.js
@@ -0,0 +1,38 @@
+/**
+ * @license Apache-2.0
+ * @copyright Julio Condor 2024
+ */
+"use strict";
+
+const cookies = new Map(
+  document.cookie.split("; ").map(item => item.split("="))
+);
+
+// base address web api
+const BASE_URL = "https://api.spotify.com/v1";
+const headers = {
+  "Authorization": `Bearer ${cookies.get("access_token")}`,
+  "Content-Type": "application/json",
+};
+
+/**
+ * Transfer playback to a new device and determine if it should start playing
+ * @param {string} deviceId - current device id,
+ * @param {Boolean} play - if true then playback happens on a new device otherwise keep
+ * the playback state
+ */
+const transferPlayback = async (deviceId, play = false) => {
+  try {
+    const reqBody = { device_ids: [deviceId], play };
+
+    await fetch(`${BASE_URL}/me/player`, {
+      method: "PUT",
+      headers,
+      body: JSON.stringify(reqBody),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { cookies, transferPlayback };
---
+++ public/js/player/client_player.js
@@ -0,0 +1,105 @@
+/**
+ * @license Apache-2.0
+ * @copyright Julio Condor 2024
+ */
+"use strict";
+
+/**
+ * custom modules
+ */
+import { cookies, transferPlayback } from "./client_player.api.js";
+
+import { addEventOnElems } from "../utils.js";
+
+
+const /**{Array<HTMLElement>} */ $players =
+    document.querySelectorAll("[data-player]");
+
+const updatePlayerInfo = (playerState, $player) => {
+  const /**{HTMLElement} */ $trackBanner = $player.querySelector(
+      "[data-track-banner]"
+    );
+  const /**{HTMLElement} */ $trackName =
+      $player.querySelector("[data-track-name]");
+  const /**{HTMLElement} */ $trackArtist = $player.querySelector(
+      "[data-track-artist]"
+    );
+
+  //destructure current playerState
+  const {
+    track_window: {
+      current_track: {
+        album: { images: trackImages },
+        artists: trackArtists,
+        name: trackName,
+      },
+    },
+  } = playerState;
+
+  console.log(playerState);
+  const {
+    url = "/images/track-banner.png",
+    width,
+    height,
+  } = trackImages.find((item) => item.width > 200 && item.width < 400);
+
+  const /** {string} */ artistNames = trackArtists
+      .map(({ name }) => name)
+      .join(", ");
+
+  /**
+   * update player image, track name, artist name and remove hide and disabled class
+   */
+  $trackBanner.src = url;
+  $trackBanner.width = width;
+  $trackBanner.height = height;
+
+  $trackBanner.alt = trackName;
+  $trackName.textContent = trackName;
+  $trackArtist.textContent = artistNames;
+  $player.classList.remove("hide");
+  $player.classList.remove("disabled");
+};
+
+/**
+ * When any changes occur in player this function will be execute
+ * e.g. change track/volume/play/pause/seek/next/previous
+ * @param {object} playerState - currently playing track object
+ */
+const playerStateChanged = (playerState) => {
+  const { track_window } = playerState;
+
+  $players.forEach((player) => updatePlayerInfo(playerState, player));
+};
+
+window.onSpotifyWebPlaybackSDKReady = () => {
+  const token = cookies.get("access_token");
+  const /**{number} */ volume = localStorage.getItem("volume") ?? 100;
+
+  /** create spotify player instance */
+  const player = new Spotify.Player({
+    name: "Soundfy Web Player",
+    getOAuthToken: (callback) => {
+      callback(token);
+    },
+    volume: volume / 100,
+  });
+
+  // Player is ready
+  player.addListener("ready", async ({ device_id }) => {
+
+    // store device_id in localStorage
+    localStorage.setItem("device_id", device_id);
+    // transfer playback
+    await transferPlayback(device_id);
+
+    const /**{Array<HTMLElement>} */ playBtns = document.querySelector('[data-play-btn]')
+
+  });
+
+  // call event when any changes occur in player
+  player.addListener("player_state_changed", playerStateChanged);
+
+  // Connect player
+  player.connect(); // aquí parece estar el problema en el connect
+};
