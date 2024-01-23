2eb70c17bed5d334310728ec362836dcc4e0ac51
Julio Condor <julioc3bgud@gmail.com>
2024-01-22 22:58:54 -0500

    Category api

--- src/api/category.api.js
+++ src/api/category.api.js
@@ -4,3 +4,28 @@
  */
 
 "use strict";
+
+/**
+ * custom modules
+ * */
+const { getData } = require("../config/axios.config");
+const { getUrlQuery } = require("../utils/helpers.util");
+
+
+/**
+ * Get a list categories used to tag items in Spotify
+ * @param {Object} req - server request object 
+ * @returns {Object}
+ */
+const getSeveralDetail = async (req) => {
+  const { limit, offset, page } = getUrlQuery(req.params);
+
+  const { data: { categories }} = await getData(`/browse/categories?limit=${limit}&offset=${offset}`, req.cookies.access_token)
+
+  return { baseUrl: req.baseUrl, page, name: 'Explore', ...categories }
+
+}
+
+module.exports = {
+  getSeveralDetail
+}
