6799e22629f9addc5cfc3dac2adaad738aa6a5a9
Julio Condor <julioc3bgud@gmail.com>
2024-02-17 16:58:33 -0500

    Add top track route

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -14,10 +14,11 @@ const router = require('express').Router()
 /**
  * Custom modules
  */
-const { profile, topArtist } = require('../controllers/profile.controller')
+const { profile, topArtist, topTrack } = require('../controllers/profile.controller')
 
 router.get('/' , profile)
 router.get(['/top/artist', '/top/artist/page/:page'] , topArtist)
+router.get(['/top/track', '/top/track/page/:page'] , topTrack)
 
 
 module.exports = router
