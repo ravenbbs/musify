dd46b94505f5114eafe876b108fa154196496fc8
Julio Condor <julioc3bgud@gmail.com>
2024-02-18 01:34:12 -0500

    Add route  - artist - track - playlist

--- src/routes/search.route.js
+++ src/routes/search.route.js
@@ -14,11 +14,13 @@ const router = require('express').Router()
 /**
  * Custom modules
  */
-const { searchRequest, searchAll, searchAlbum } = require('../controllers/search.controller')
+const { searchRequest, searchAll, searchAlbum, searchArtist, searchTracks, searchPlaylist } = require('../controllers/search.controller')
 
 router.post('/', searchRequest )
 router.get('/all/:query', searchAll )
 router.get(['/albums/:query', '/albums/:query/page/:page'], searchAlbum)
-// router.get(['/playlist/:query', '/playlist/:query/page/:page'], searchPlaylist)
+router.get(['/artists/:query', '/artists/:query/page/:page'], searchArtist)
+router.get(['/tracks/:query', '/tracks/:query/page/:page'], searchTracks)
+router.get(['/playlists/:query', '/playlists/:query/page/:page'], searchPlaylist)
 
 module.exports = router
