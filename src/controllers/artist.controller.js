ad543475cd5179207a5095dd8d10b13228d12282
Julio Condor <julioc3bgud@gmail.com>
2024-02-19 01:24:30 -0500

    Add artist controller - artist albums

--- src/controllers/artist.controller.js
+++ src/controllers/artist.controller.js
@@ -4,7 +4,6 @@
  */
 "use strict";
 
-
 /**
  * custom modules
  */
@@ -13,27 +12,67 @@ const playerApi = require("../api/player.api");
 const apiConfig = require("../config/api.config");
 const artistApi = require("../api/artist.api");
 
-const { msToTimeCode } = require('../utils/helpers.util');
+const { msToTimeCode } = require("../utils/helpers.util");
 
 const artistDetail = async (req, res) => {
-      //current user profile
-      const currentProfile = await userApi.getProfile(req);
+  //current user profile
+  const currentProfile = await userApi.getProfile(req);
 
-      //recently played
-      const recentlyPlayed = await playerApi.getRecentlyPlayed(req);
-      const recentlyPlayedTracks = await recentlyPlayed.items.map(
-        ({ track }) => track
-      );
+  //recently played
+  const recentlyPlayed = await playerApi.getRecentlyPlayed(req);
+  const recentlyPlayedTracks = await recentlyPlayed.items.map(
+    ({ track }) => track
+  );
 
+  // artist detail
+  const artistAlbums = await artistApi.getAlbum(req, apiConfig.LOW_LIMIT);
 
-      res.render("./pages/artist_detail", {
-        currentProfile,
-        recentlyPlayedTracks,
-      
-      });
+  //artist detail
+  const artistDetail = await artistApi.getDetail(req);
+
+  //artist top tracks
+  const artistTopTracks = await artistApi.getTopTracks(req);
+
+  //artist related artist
+  const relatedArtist = await artistApi.getRelated(req)
+
+  res.render("./pages/artist_detail", {
+    currentProfile,
+    recentlyPlayedTracks,
+    artistAlbums,
+    artistDetail,
+    artistTopTracks,
+    relatedArtist,
+    msToTimeCode
+  });
+};
+
+const artistAlbum = async (req, res) => {
+    //current user profile
+    const currentProfile = await userApi.getProfile(req);
+
+    //recently played
+    const recentlyPlayed = await playerApi.getRecentlyPlayed(req);
+    const recentlyPlayedTracks = await recentlyPlayed.items.map(
+      ({ track }) => track
+    );
+
+    // artist detail
+    const artistAlbums = await artistApi.getAlbum(req);
+    
+    //artist detail
+    const artistDetail = await artistApi.getDetail(req);
+
+    res.render("./pages/album", {
+      currentProfile,
+      recentlyPlayedTracks,
+      title: artistDetail.name,
+      albums: artistAlbums,
+      isArtistAlbum: true
+    });
 }
-
 
 module.exports = {
-  artistDetail
-}
+  artistDetail,
+  artistAlbum
+};
