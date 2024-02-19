a4c318d2acbd4854f7ad7e50f58cfd4fb9384278
Julio Condor <julioc3bgud@gmail.com>
2024-02-19 01:26:49 -0500

    Add Artist api - get detail - Top tracks - related

--- src/api/artist.api.js
+++ src/api/artist.api.js
@@ -6,11 +6,11 @@
 "use strict";
 
 /**
- * custom modules 
+ * custom modules
  * */
 const apiConfig = require("../config/api.config");
-const { getData } = require('../config/axios.config')
-const { getUrlQuery } = require('../utils/helpers.util');
+const { getData } = require("../config/axios.config");
+const { getUrlQuery } = require("../utils/helpers.util");
 
 /**
  * Get Spotify catalog information for several artist based on their Spotify IDs
@@ -20,9 +20,12 @@ const { getUrlQuery } = require('../utils/helpers.util');
  * @returns {Object}
  */
 const getSeveralDetail = async (req, artistIds) => {
-  const {data: trackArtists } = await getData(`/artists?ids=${artistIds}`, req.cookies.access_token)
-  return  trackArtists
-}
+  const { data: trackArtists } = await getData(
+    `/artists?ids=${artistIds}`,
+    req.cookies.access_token
+  );
+  return trackArtists;
+};
 
 /**
  * Get spotify catalog information about an artist albums
@@ -31,19 +34,72 @@ const getSeveralDetail = async (req, artistIds) => {
  * @param {string} id - the spotify ID of the artist
  */
 const getAlbum = async (req, itemLimit, id) => {
-  const {offset, limit, page} = getUrlQuery(req.params, itemLimit)
-  const { artistId = id} = req.params;
+  const { offset, limit, page } = getUrlQuery(req.params, itemLimit);
+  const { artistId = id } = req.params;
 
-  const { data: artistAlbum } = await getData(`/artists/${artistId}/albums?limit=${limit}&offset=${offset}`, req.cookies.access_token)
+  const { data: artistAlbum } = await getData(
+    `/artists/${artistId}/albums?limit=${limit}&offset=${offset}`,
+    req.cookies.access_token
+  );
 
-  const /** {string} */ baseUrl = `${req.baseUrl}/${artistId}/album`
+  const /** {string} */ baseUrl = `${req.baseUrl}/${artistId}/album`;
 
-  return { baseUrl, page, ...artistAlbum}
+  return { baseUrl, page, ...artistAlbum };
+};
 
-}
+/**
+ * Get spotify catalog information for a single artist identified by their unique spotify ID
+ * @param {Object} req - server request object
+ */
+const getDetail = async (req) => {
+  const { artistId } = req.params;
 
+  const { data: artistDetail } = await getData(
+    `/artists/${artistId}`,
+    req.cookies.access_token
+  );
+
+  return artistDetail;
+};
+
+/**
+ * Get spotify catalog information about an artists top tracks
+ * @param {Object} req - server request object
+ * @param {string} id - the spotify ID of the artist
+ * @returns {Object}
+ */
+const getTopTracks = async (req, id) => {
+  const { artistId = id } = req.params;
+
+  const { data: artistTopTracks } = await getData(
+    `/artists/${artistId}/top-tracks?market=${apiConfig.MARKET}`,
+    req.cookies.access_token
+  );
+
+  return artistTopTracks;
+};
+
+/**
+ * Get spotify catalog information about artists similar to a given artist. Similarity is based on analysis of the Spotify community's listening history
+ * @param {Object} req - server request object
+ * @param {string} id - the spotify ID of the artist
+ * @returns {Object}
+ */
+const getRelated = async (req, id) => {
+  const { artistId = id } = req.params;
+
+  const { data: relatedArtist } = await getData(
+    `/artists/${artistId}/related-artists`,
+    req.cookies.access_token
+  );
+
+  return relatedArtist;
+};
 
 module.exports = {
   getSeveralDetail,
-  getAlbum
-}
+  getAlbum,
+  getDetail,
+  getTopTracks,
+  getRelated
+};
